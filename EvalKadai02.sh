2b04c8cc207cfa81d00eb902398f794fbc1efa24
HiKat <pekumrp@gmail.com>
2015-11-03 15:23:08 +0900

    save

--- EvalKadai02.sh
+++ EvalKadai02.sh
@@ -14,7 +14,7 @@ WRL4=sample/av4.wrl
 PPM4=Kadai02ForAv4.ppm
 
 WRLhead=sample/head.wrl
-PPM4head=Kadai02ForHead.ppm
+PPMhead=Kadai02ForHead.ppm
 
 WRL1997=sample/iiyama1997.wrl
 PPM1997=Kadai02ForIiyama1997.ppm
@@ -24,14 +24,19 @@ echo start!!
 gcc -Wall $SRC
 ./a.out $WRL1 $PPM1
 open $PPM1
+
 ./a.out $WRL2 $PPM2
 open $PPM2
+
 ./a.out $WRL3 $PPM3
 open $PPM3
+
 ./a.out $WRL4 $PPM4
 open $PPM4
+
 ./a.out $WRLhead $PPMhead
 open $PPMhead
+
 ./a.out $WRL1997 $PPM1997
 open $PPM1997
 
